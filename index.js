543d70dcfbde0aa5925d769f4ac07569515ae8cd
SFarida <faridagueleu@gmail.com>
2022-12-21 00:04:28 +0100

    Add modal

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function toggleMenu() {
   }
 }
 
-toggleMenu();
+// toggleMenu();
 
 function hideMenu() {
   document.getElementById('close_btn').classList.remove('show');
@@ -29,4 +29,68 @@ function hideMenu() {
   document.getElementById('menuIcon').classList.add('menu');
 }
 
-hideMenu();
+// hideMenu();
+
+const projects = [
+    {
+      id: '1',
+      name: 'Profesional Art Printing Data 1',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standards',
+      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    },
+    {
+      id: '2',
+      name: 'Profesional Art Printing Data 2',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    },
+    {
+      id: '3',
+      name: 'Profesional Art Printing Data 3',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    },
+    {
+      id: '4',
+      name: 'Profesional Art Printing Data 4',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    },
+    {
+      id: '5',
+      name: 'Profesional Art Printing Data 5',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    },
+    {
+      id: '6',
+      name: 'Profesional Art Printing Data 6',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    },
+  ];
+
+
+  // Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+function closeModal(){
+    document.getElementsByClassName("close")[0].style.display = "none";
+}
+
+// When the user clicks the button, open the modal 
+function openModal() {
+    console.log("modal is ", modal)
+    document.getElementById("myModal").style.display = "block";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
