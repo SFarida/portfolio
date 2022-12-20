95a86fed75ccfc75b364ce58267685c1036c2a68
SFarida <faridagueleu@gmail.com>
2022-12-20 21:08:57 +0100

    Add toggle function for responsive menu

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function toggleMenu(){
-    console.log("hey there !")
+function toggleMenu() {
+  const menu = document.getElementById('menuIcon');
+  const header = document.getElementById('nav_');
+  const closeButton = document.getElementById('close_btn');
+  const toggleBars = document.getElementById('menu_hamburger');
+
+  if (!(menu.classList.contains('mobile_menu'))) {
+    closeButton.classList.add('show');
+    toggleBars.classList.add('hide');
+    menu.classList.add('mobile_menu');
+    header.classList.add('mobile_header');
+    menu.classList.remove('menu');
+  } else {
+    closeButton.classList.remove('show');
+    toggleBars.classList.remove('hide');
+    menu.classList.remove('mobile_menu');
+    header.classList.remove('mobile_header');
+    menu.classList.add('menu');
+  }
 }
+
+toggleMenu();
