f80b8706a6967209be1dc7f91d98a5262d60793d
SFarida <faridagueleu@gmail.com>
2022-12-22 21:24:20 +0100

    Validate form with js

--- index.js
+++ index.js
@@ -32,65 +32,88 @@ function hideMenu() {
 // hideMenu();
 
 const projects = [
-    {
-      id: '1',
-      name: 'Profesional Art Printing Data 1',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standards',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
-    },
-    {
-      id: '2',
-      name: 'Profesional Art Printing Data 2',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
-    },
-    {
-      id: '3',
-      name: 'Profesional Art Printing Data 3',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
-    },
-    {
-      id: '4',
-      name: 'Profesional Art Printing Data 4',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
-    },
-    {
-      id: '5',
-      name: 'Profesional Art Printing Data 5',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
-    },
-    {
-      id: '6',
-      name: 'Profesional Art Printing Data 6',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
-    },
-  ];
+  {
+    id: '1',
+    name: 'Profesional Art Printing Data 1',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standards',
+    cardSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '2',
+    name: 'Profesional Art Printing Data 2',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    cardSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '3',
+    name: 'Profesional Art Printing Data 3',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    cardSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '4',
+    name: 'Profesional Art Printing Data 4',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    cardSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '5',
+    name: 'Profesional Art Printing Data 5',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    cardSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '6',
+    name: 'Profesional Art Printing Data 6',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    cardSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+];
 
 
-  // Get the modal
+// Get the modal
 let modal = document.getElementById("myModal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-function closeModal(){
-    document.getElementsByClassName("close")[0].style.display = "none";
+function closeModal() {
+  document.getElementsByClassName("close")[0].style.display = "none";
 }
 
 // When the user clicks the button, open the modal 
 function openModal() {
-    console.log("modal is ", modal)
-    document.getElementById("myModal").style.display = "block";
-  }
+  document.getElementById("myModal").style.display = "block";
+}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+window.onload = function () {
+  document.getElementById('contact_form').addEventListener('submit', validateForm)
+};
+
+
+function validateForm(e) {
+  let email = document.getElementById('form_input2').value;
+  let islowercase = checkLowerCase(email);
+  let form = document.getElementById('contact_form')
+  if (islowercase) {
+    form.action = 'https://formspree.io/f/myyvrjpe'
+    form.submit();
+  } else {
+    document.getElementById('validation_error_msg').innerHTML = "Your email address should be in lowercase"
+  }
+}
+
+function checkLowerCase(str) {
+  if (str == str.toLowerCase()) {
+    return true;
+  } else {
+    return false;
   }
 }
