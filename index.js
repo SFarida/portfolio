fb97421fe038abec67b40112c5ea685d064325e7
SFarida <faridagueleu@gmail.com>
2022-12-22 17:31:39 +0100

    Add work section contain dynamically from js and popup display

--- index.js
+++ index.js
@@ -32,65 +32,281 @@ function hideMenu() {
 // hideMenu();
 
 const projects = [
-    {
-      id: '1',
-      name: 'Profesional Art Printing Data 1',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standards',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
+  {
+    id: '1',
+    title: 'Profesional Art Printing Data 1',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standards',
+    projectSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '2',
+    title: 'Profesional Art Printing Data 2',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    projectSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '3',
+    title: 'Profesional Art Printing Data 3',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    projectSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '4',
+    title: 'Profesional Art Printing Data 4',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    projectSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '5',
+    title: 'Profesional Art Printing Data 5',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    projectSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+  {
+    id: '6',
+    title: 'Profesional Art Printing Data 6',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    projectSkills: ['html', 'bootstrap', 'Ruby'],
+  },
+];
+
+const project_details = [
+  {
+    id: 1,
+    image: 'img/snapshoot_Portfolio.png',
+    title: 'Keeping track of hundreds of components website 1',
+    titlesmall: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releax map lapora verita`,
+    technologies: {
+      tech1: 'html',
+      tech2: 'Bootstrap',
+      tech3: 'Ruby on rails',
     },
-    {
-      id: '2',
-      name: 'Profesional Art Printing Data 2',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    LinkToLive: 'https://sfarida.github.io/portfolio/',
+    LinkToSource: 'https://github.com/SFarida/portfolio/',
+  },
+  {
+    id: 2,
+    image: 'img/snapshoot_Portfolio.png',
+    title: 'Keeping track of hundreds of components website 2',
+    titlesmall: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releax map lapora verita`,
+    technologies: {
+      tech1: 'html',
+      tech2: 'Bootstrap',
+      tech3: 'Ruby on rails',
     },
-    {
-      id: '3',
-      name: 'Profesional Art Printing Data 3',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    LinkToLive: 'https://sfarida.github.io/portfolio/',
+    LinkToSource: 'https://github.com/SFarida/portfolio/',
+  },
+  {
+    id: 3,
+    image: 'img/snapshoot_Portfolio.png',
+    title: 'Keeping track of hundreds of components website 3',
+    titlesmall: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releax map lapora verita`,
+    technologies: {
+      tech1: 'html',
+      tech2: 'Bootstrap',
+      tech3: 'Ruby on rails',
     },
-    {
-      id: '4',
-      name: 'Profesional Art Printing Data 4',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    LinkToLive: 'https://sfarida.github.io/portfolio/',
+    LinkToSource: 'https://github.com/SFarida/portfolio/',
+  },
+  {
+    id: 4,
+    image: './imgnew/ImgPlaceholder(2).png',
+    title: 'Keeping track of hundreds of components website 4',
+    titlesmall: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releax map lapora verita`,
+    technologies: {
+      tech1: 'html',
+      tech2: 'Bootstrap',
+      tech3: 'Ruby on rails',
     },
-    {
-      id: '5',
-      name: 'Profesional Art Printing Data 5',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    LinkToLive: 'https://sfarida.github.io/portfolio/',
+    LinkToSource: 'https://github.com/SFarida/portfolio/',
+  },
+  {
+    id: 5,
+    image: './imgnew/ImgPlaceholder(2).png',
+    title: 'Keeping track of hundreds of components website 5',
+    titlesmall: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releax map lapora verita`,
+    technologies: {
+      tech1: 'html',
+      tech2: 'Bootstrap',
+      tech3: 'Ruby on rails',
     },
-    {
-      id: '6',
-      name: 'Profesional Art Printing Data 6',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
-      cardSkills: ['html', 'bootstrap', 'Ruby'],
+    LinkToLive: 'https://sfarida.github.io/portfolio/',
+    LinkToSource: 'https://github.com/SFarida/portfolio/',
+  },
+  {
+    id: 6,
+    image: 'img/snapshoot_Portfolio.png',
+    title: 'Keeping track of hundreds of components website 6',
+    titlesmall: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releax map lapora verita`,
+    technologies: {
+      tech1: 'html',
+      tech2: 'Bootstrap',
+      tech3: 'Ruby on rails',
     },
-  ];
+    LinkToLive: 'https://sfarida.github.io/portfolio/',
+    ILinkToSource: 'https://github.com/SFarida/portfolio/',
+  },
+  {
+    id: 7,
+    image: 'img/snapshoot_Portfolio.png',
+    title: 'Multi-Post Stories',
+    titlesmall: 'Multi-Post Stories',
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it 1960s with the 
+      releax map lapora verita`,
+    technologies: {
+      tech1: 'html',
+      tech2: 'Bootstrap',
+      tech3: 'Ruby on rails',
+    },
+    LinkToLive: 'https://sfarida.github.io/portfolio/',
+    ILinkToSource: 'https://github.com/SFarida/portfolio/',
+  },
+];
 
 
-  // Get the modal
+// Get the modal
 let modal = document.getElementById("myModal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-function closeModal(){
-    document.getElementsByClassName("close")[0].style.display = "none";
+
+// When the user clicks on (x), close the modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
 }
 
 // When the user clicks the button, open the modal 
 function openModal() {
-    console.log("modal is ", modal)
-    document.getElementById("myModal").style.display = "block";
-  }
+  const modal = document.getElementById('myModal')
+  const project_detail = project_details[project_details.length - 1]
+  const header = document.createTextNode(project_detail.title)
+  modal.childNodes[3].childNodes[3].childNodes[1].removeChild(modal.childNodes[3].childNodes[3].childNodes[1].childNodes[0])
+  modal.childNodes[3].childNodes[3].childNodes[1].appendChild(header)
+  document.getElementById("myModal").style.display = "block";
+}
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+const loadProjects = () => {
+  const container = document.getElementById('other_works');
+  for (let i = 0; i < projects.length; i++) {
+    // Creating the divs
+    const other_works_item_div = document.createElement('div');
+    const hover_action_div = document.createElement('div');
+    const right_block_div = document.createElement('div');
+    const heading = document.createElement('h3');
+    const paragraph = document.createElement('p');
+    const action_div = document.createElement('div');
+    const button = document.createElement('button')
+    const list = document.createElement('ul');
+    const button_span = document.createElement('span');
+    let list_items = [];
+
+    // Creating text nodes
+    h3_text = document.createTextNode(projects[i].title);
+    p_text = document.createTextNode(projects[i].description);
+    html_text = document.createTextNode(projects[i].projectSkills[0]);
+    boostrap_text = document.createTextNode(projects[i].projectSkills[1]);
+    ruby_text = document.createTextNode(projects[i].projectSkills[2]);
+    button_text = document.createTextNode('See Project')
+
+    // Adding the classes
+    other_works_item_div.classList.add('other_works_item1', 'other_works_item');
+    hover_action_div.classList.add('hover_action');
+    right_block_div.classList.add('right_block');
+    action_div.classList.add('action')
+    button.classList.add('see_project_action_btn')
+    list.classList.add('projects_stack')
+    button_span.classList.add('btn_text')
+
+    // Appending the n+1 child to each div
+    heading.appendChild(h3_text);
+    paragraph.appendChild(p_text);
+    // Loop through projecSkills to add text to list items
+    for (let j = 0; j < projects[i].projectSkills.length; j++) {
+      let list_item = document.createElement('li');
+      list_item.classList.add('projects_stack_item', 'projects_stack_item1')
+      let span = document.createElement('span');
+      let text = document.createTextNode(projects[i].projectSkills[j])
+      span.appendChild(text)
+      list_item.appendChild(span)
+      list_items.push(list_item)
+    }
+    for (let j = 0; j < list_items.length; j++) {
+      list.appendChild(list_items[j])
+    }
+    other_works_item_div.appendChild(hover_action_div);
+    other_works_item_div.appendChild(action_div);
+    button.appendChild(button_span);
+    action_div.appendChild(button);
+    button_span.appendChild(button_text)
+    hover_action_div.appendChild(right_block_div);
+    right_block_div.appendChild(heading);
+    right_block_div.appendChild(paragraph);
+    right_block_div.appendChild(list);
+
+    // button.addEventListener('onclick', openModal(projects[i].id))
+    // button.onclick = openModal(projects[i].id)
+
+    container.appendChild(other_works_item_div)
+  }
+}
+
+
+window.onload = function () {
+  loadProjects();
+};
